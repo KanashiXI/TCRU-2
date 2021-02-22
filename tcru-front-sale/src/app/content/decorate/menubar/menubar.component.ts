f556e7e22440f85a3d585f83c7795d6f923542de
KanashiXI <panyapat.test@gmail.com>
2021-02-23 02:32:42 +0700

    edit menubar

--- tcru-front-sale/src/app/content/decorate/menubar/menubar.component.ts
+++ tcru-front-sale/src/app/content/decorate/menubar/menubar.component.ts
@@ -3,6 +3,9 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/service/auth.service';
+import { TokenService } from 'src/app/shared/service/token.service';
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
@@ -10,12 +13,30 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MenubarComponent {
 
+  public loggedIn: boolean;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private Auth: AuthService,
+    private router: Router,
+    private Token: TokenService
+    ) {}
+
+    logout(event: MouseEvent) {
+      event.preventDefault();
+      this.Token.remove();
+      this.Auth.changeAuthStatus(false);
+      this.router.navigateByUrl('/login');
+  }
+
+  ngOnInit(): void {
+    this.Auth.authStatus.subscribe(value => this.loggedIn = value);
+  }
 
 }
