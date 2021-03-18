5b5fff5053f12bcdce30b1d53120e6e8f4e0f98f
KanashiXI <panyapat.test@gmail.com>
2021-03-18 17:28:26 +0700

    Badge cart

--- tcru-front-sale/src/app/content/decorate/menubar/menubar.component.ts
+++ tcru-front-sale/src/app/content/decorate/menubar/menubar.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/service/auth.service';
 import { TokenService } from 'src/app/shared/service/token.service';
+import { CartService } from 'src/app/shared/service/cart.service';
+import { Product } from '../../shop/shopview/interfaces/product';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
@@ -14,6 +17,16 @@ import { TokenService } from 'src/app/shared/service/token.service';
 export class MenubarComponent {
 
   public loggedIn: boolean;
+
+  sumProductInCart: Number = 0;
+  productInCart: Product[] = [];
+  cartTotal: number = 0;
+
+
+  loadUpdateCart: boolean = false;
+  editProductQuantityForm: FormGroup;
+  reactiveForm: FormGroup;
+
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -25,7 +38,10 @@ export class MenubarComponent {
     private breakpointObserver: BreakpointObserver,
     private Auth: AuthService,
     private router: Router,
-    private Token: TokenService
+    private Token: TokenService,
+    private cartService: CartService,
+    private fb: FormBuilder,
+
     ) {}
 
     logout(event: MouseEvent) {
@@ -37,7 +53,40 @@ export class MenubarComponent {
 
   ngOnInit(): void {
     this.Auth.authStatus.subscribe(value => this.loggedIn = value);
+    const requestData = {
+      ...Subject,
+      customerUsername: localStorage.getItem('user_id'),
+    }
+    this.queryCartProduct(requestData.customerUsername);
   }
 
+  createForm(uId) {
+    this.reactiveForm = this.fb.group({
+      order_id: ['',],
+    })
+
+    this.reactiveForm.patchValue({
+      user_id: uId,
+    })
+
+    this.editProductQuantityForm = this.fb.group({
+      product_id: [''],
+      product_quantity: [''],
+      user_id: [''],
+    })
+  }
+
+  queryCartProduct(user_id) {
+    this.cartService.getCartItemList(user_id).subscribe(res => {
+      this.productInCart = res;
+    })
+  }
+
+  ngAfterContentChecked() {
+    this.cartTotal = 0;
+    this.productInCart.map((obj) => {
+      this.cartTotal += Number(obj.product_quantity);
+    });
+  }
 
 }
