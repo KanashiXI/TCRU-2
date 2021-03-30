8df945fab3de485b56ce5ae56cb8e5da4ebfe9ee
mekpongsathon <Pongsathon.Rakthong@gmail.com>
2021-03-30 15:32:04 +0700

    cart calculate

--- tcru-front-sale/src/app/content/shop/cart/cart.component.ts
+++ tcru-front-sale/src/app/content/shop/cart/cart.component.ts
@@ -32,11 +32,11 @@ import { Shippingcost } from './../../../shared/interface/shippingcost';
   styleUrls: ['./cart.component.css'],
   animations: [
     trigger('fadeInOut', [
-      transition(':enter', [   // :enter is alias to 'void => *'
+      transition(':enter', [
         style({ opacity: 0 }),
         animate(500, style({ opacity: 1 }))
       ]),
-      transition(':leave', [   // :leave is alias to '* => void'
+      transition(':leave', [
         animate(500, style({ opacity: 0 }))
       ])
     ])
@@ -88,44 +88,7 @@ export class CartComponent implements OnInit {
   cartWeight: number;
   sumShippingCost: number = 0;
   @ViewChild('htmlData') htmlData: ElementRef;
-  USERS = [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "email": "sincere@april.biz",
-      "phone": "1-770-736-8031 x56442"
-    },
-    {
-      "id": 2,
-      "name": "Ervin Howell",
-      "email": "shanna@melissa.tv",
-      "phone": "010-692-6593 x09125"
-    },
-    {
-      "id": 3,
-      "name": "Clementine Bauch",
-      "email": "nathan@yesenia.net",
-      "phone": "1-463-123-4447",
-    },
-    {
-      "id": 4,
-      "name": "Patricia Lebsack",
-      "email": "julianne@kory.org",
-      "phone": "493-170-9623 x156"
-    },
-    {
-      "id": 5,
-      "name": "Chelsey Dietrich",
-      "email": "lucio@annie.ca",
-      "phone": "(254)954-1289"
-    },
-    {
-      "id": 6,
-      "name": "Mrs. Dennis",
-      "email": "karley@jasper.info",
-      "phone": "1-477-935-8478 x6430"
-    }
-  ];
+
   constructor(
     private router: Router,
     private addressService: AddressService,
@@ -396,11 +359,9 @@ export class CartComponent implements OnInit {
         promotion_id: this.promotionId,
         total_price: this.totalPrice,
       })
-
     } else {
       this.isGotPromotion = false;
       this.calculateShippingCostByWeight(this.cartWeight)
-
       this.reactiveForm.patchValue({
         discount: this.discount,
         net_amount: this.cartTotal,
@@ -411,12 +372,9 @@ export class CartComponent implements OnInit {
   }
 
   calculateShippingCostByWeight(cartWeight) {
-    this.shippingData.sort((a, b) => b.weight_condition - a.weight_condition);
-
-    this.shippingData.filter((value) => {
+    this.shippingData.sort((a, b) => b.weight_condition - a.weight_condition).filter((value) => {
       if (Number(cartWeight) <= Number(value.weight_condition)) {
         this.sumShippingCost = value.transportation_cost;
-        console.log(cartWeight + '----- <= -----' + value.weight_condition)
       }
     })
 
