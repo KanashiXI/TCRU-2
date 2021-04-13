eeeb6df269f2f5ab386800348195f97b665b46a7
Fahg2541 <Fahg2541@gmail.com>
2021-04-14 00:41:08 +0700

    show page and delete

--- tcru-front-staff/src/app/OrderMaterials/show-order/show-order.component.ts
+++ tcru-front-staff/src/app/OrderMaterials/show-order/show-order.component.ts
@@ -3,6 +3,7 @@ import { OrderMaterialsService } from '../../Service/order-materials.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { OrderMaterials } from '../../Models/OrderMaterials.model';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-show-order',
@@ -17,9 +18,15 @@ export class ShowOrderComponent implements OnInit {
   materialMessage: string;
   dataArr: any;
   material_name:any;
+  page: any = 1;
+  limit: any = 5;
+  skip: any;
+  totalCount: any;
+
   constructor(private http: HttpClient,
     private Order: OrderMaterialsService,
-    private route: ActivatedRoute,) { }
+    private route: ActivatedRoute,
+    private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.getOrder();
@@ -27,9 +34,33 @@ export class ShowOrderComponent implements OnInit {
 
   getOrder()
   {
-    this.Order.getData().subscribe(res=>{
-      this.dataArr=res;
-    })
+    this.spinner.show();
+    if(this.page == 1){
+      this.skip = 0;
+    }
+    else {
+      this.skip = (this.page-1) * this.limit;
+    }
+    var requestObj = {
+      'limit': this.limit,
+      'skip': this.skip
+    }
 
-}
+    this.Order.getData1(requestObj).subscribe((res:any) => {
+      this.spinner.hide();
+      this.dataArr = res.data;
+      this.totalCount = res.totalRecord;
+    })
+  }
+
+  deleteOrderM(id)
+  {
+    if(confirm('คุณต้องการลบฟรือไม่ ?') == true)
+    this.Order.deleteOrderM(id).subscribe(result => {
+      this.getOrder();
+    },
+    err => {
+      console.log(err);
+    })
+  }
 }
