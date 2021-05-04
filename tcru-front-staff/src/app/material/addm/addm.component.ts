8f75e922c5a33cf7d683c90ec850a16956a9f65e
namfon317 <35970261+namfon317@users.noreply.github.com>
2021-05-05 00:15:59 +0700

    add

--- tcru-front-staff/src/app/material/addm/addm.component.ts
+++ tcru-front-staff/src/app/material/addm/addm.component.ts
@@ -4,9 +4,11 @@ import { Materialinterface } from '../../interfaces/materialinterface';
 import { MeterialserviceService } from '../../service/meterialservice.service';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from "ngx-spinner";
+import { Material } from '../../models/Material.model';
 // import { reverse } from 'dns';
 
 @Component({
@@ -18,18 +20,20 @@ export class AddmComponent {
 
   materialList: any[]=[];
   imageDirectoyPath:any = 'http://127.0.0.1:5000/public/image/';
-  material_id: string;
   searchText: string;
   Search: string;
   materialMessage: string;
   dataArr: any;
-  material_name:any;
   page:any = 1;
   limit: any = 5;
   skip: any;
   categoryArr:any;
   totalCount: any;
-  constructor(private http: HttpClient,
+  Materials = new Material();
+  data: any;
+  form: FormGroup;
+  MaterialCategory: any;
+  constructor(private fb: FormBuilder, private http: HttpClient,
     private MeterialService: MeterialserviceService,
     private toastr: ToastrService,
     private route: ActivatedRoute,
@@ -39,6 +43,20 @@ export class AddmComponent {
   }
   ngOnInit(): void {
     this.getMeterial();
+    this.getData();
+    this.getcategory();
+  }
+  createForm() {
+    this.form = this.fb.group({
+      // material_id: ['', [Validators.required]],
+      material_name: ['', [Validators.required], ],
+      weight: ['', [Validators.required]],
+      category_id: ['', [Validators.required]],
+      image: ['', [Validators.required]],
+      unit_count_id: ['', [Validators.required]],
+      scale: ['', [Validators.required]],
+      critical: ['', [Validators.required]],
+    });
   }
   getMeterial()
   {
@@ -57,6 +75,42 @@ export class AddmComponent {
       this.dataArr=res.data;
       this.totalCount = res.totalRecord;
     })
+  }
+  getcategory() {
+    this.MeterialService.getcategory().subscribe(res => {
+      this.categoryArr = res;
+      console.log(this.categoryArr)
+    })
+  }
+
+  getData() {
+    this.MeterialService.getData().subscribe(res => {
+      this.data = res;
+      console.log(this.data)
+    })
+  }
+  getMeterialService(event) {
+    var obj = {
+      category_id: event.target.value
+
+    }
+    let x = obj.category_id
+    if (x != "") {
+      
+        this.MeterialService.MaterialCategory(obj).subscribe(res => {
+          this.MaterialCategory = res;
+          console.log(obj)
+
+      });
+    } else
+    {
+      this.MeterialService.getData().subscribe(res => {
+        this.MaterialCategory = res;
+        console.log(x)
+      
+      });
+    }
+    console.log(this.category_id)
   }
 
   deleteMaterial(id){
@@ -82,6 +136,31 @@ export class AddmComponent {
 //     this.dataSource.paginator.firstPage();
 //   }
 // }
+get material_name() {
+  return this.form.get('material_name')
+}
+get weight() {
+  return this.form.get('weight')
+}
+get material_id() {
+  return this.form.get('material_id')
+}
+get category_id() {
+  return this.form.get('category_id')
+}
+get image() {
+  return this.form.get('image')
+}
+get scale() {
+  return this.form.get('scale')
+}
+get unit_count_id() {
+  return this.form.get('unit_count_id')
+}
+get critical() {
+  return this.form.get('critical')
+}
+
 
 
 }
