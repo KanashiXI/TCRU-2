9b39ab0343b9938c5b7fd028a5f816cbfccd85fb
KanashiXI <panyapat.test@gmail.com>
2021-05-05 22:35:56 +0700

    -

---
+++ tcru-front-staff/src/app/TCRU/shipping/printAddress/printAddress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-printAddress',
+  templateUrl: './printAddress.component.html',
+  styleUrls: ['./printAddress.component.scss']
+})
+export class PrintAddressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
