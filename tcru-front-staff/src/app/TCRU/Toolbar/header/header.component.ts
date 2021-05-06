53c498102c983336b422ee3581b3ad594d43d419
Fahg2541 <Fahg2541@gmail.com>
2021-05-07 06:37:49 +0700

    ...

--- tcru-front-staff/src/app/TCRU/Toolbar/header/header.component.ts
+++ tcru-front-staff/src/app/TCRU/Toolbar/header/header.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, FormBuilder, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { JarwisService } from '../../../Service/jarwis.service';
+import { TokenService } from '../../../Service/token.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../Service/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public loggedIn: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private Auth: AuthService,
+    private router: Router,
+    private Token: TokenService
+  ) { }
+  ngOnInit() {
+    this.Auth.authStatus.subscribe(value => this.loggedIn = value);
+  }
+
+  logout(event: MouseEvent) {
+    event.preventDefault();
+    this.Token.romove();
+    this.Auth.changeAuthStatus(false);
+    this.router.navigateByUrl('/login');
   }
 
 }
